96e6afd76e4ed7269aa0fe4b1503baaa03b20c67
Nsdominguezr <ing.nahumdominguez@gmail.com>
2026-04-27 00:50:53 -0500

    COnfiguración de Rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', loadComponent: () => import('./demo/pages/auth/login/login.component').then(m => m.LoginComponent) },
+  { path: 'registro', loadComponent: () => import('./demo/pages/auth/registro/registro.component').then(m => m.RegistroComponent) },
+  {
+    path: 'home',
+    loadComponent: () => import('./demo/pages/navegacion/navegacion.component').then(m => m.NavegacionComponent),
+    canActivate: [AuthGuard]
+  },
+  { path: '**', redirectTo: '/login' }
+];
